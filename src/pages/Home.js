4f6eab877f3cef25ca41a23b6ca37b32cbc421b7
PARK GA IN <84880886+PARKGAIN@users.noreply.github.com>
2022-11-20 18:50:36 +0900

    feat : fetch api로  데이터 받아옴

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 function Home() {
-  let { data } = fetch("http://localhost:8888/users")
-    .then((Response) => Response.json())
-    .then((json) => console.log(json));
+  async function getData() {
+    const response = await fetch("http://localhost:8888/users");
+    const data = await response.json();
+    console.log(data[0]);
+    document.getElementById("name").textContent = data[0].name;
+  }
 
-  return <>{data}</>;
+  getData();
+  return (
+    <div>
+      <h4>사용자이름</h4>
+      <h5 id="name"></h5>
+    </div>
+  );
 }
 
 export default Home;
