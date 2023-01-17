12aba2452ed42d16d5bb787d36b2a31ca4286bae
parkgain <pgi082247@gmail.com>
2023-01-17 11:09:28 +0900

    docs : 경로 수정

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/join" element={<Join />} />
       </Routes>
